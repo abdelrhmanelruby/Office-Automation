9610c7b7a8bfdb0b2c9a5721e6503695092b2e44
Abdelrhman Elruby <abdelrhmanelruby@gmail.com>
2022-08-09 06:39:37 +0200

    Update firebase.ino

--- Arduino/esp8266/firebase.ino
+++ Arduino/esp8266/firebase.ino
@@ -5,20 +5,20 @@
 #include <ESP8266HTTPClient.h>
 #include <DHT.h>
 
-#define FIREBASE_HOST "nodemcu-55328-default-rtdb.firebaseio.com"                          // database URL 
-#define FIREBASE_AUTH "E89ghB29OkuptSBQkCHn0pSU9xL3pT4jNcJWl5RO"             // secret key
+#define FIREBASE_HOST "********"                         
+#define FIREBASE_AUTH "********"             
 
-#define WIFI_SSID "Ruby"
-#define WIFI_PASSWORD "24938274_@a!t!m!w!"
-#define DHTPIN 2  //be careful the pin no. on the nodeMcu and arduino ide differ 
-#define DHTTYPE DHT11 //specifying the type of sensor
+#define WIFI_SSID "********"
+#define WIFI_PASSWORD "********"
+#define DHTPIN 2  
+#define DHTTYPE DHT11 
 
 DHT dht(DHTPIN, DHTTYPE);
 
 bool light;
 bool rgb;
 int r, g, b;
-unsigned long previousMillis = 0; //for adding a timelimit after which the data will be read
+unsigned long previousMillis = 0; 
 const long interval = 10000;
 void setup()
 {
@@ -28,8 +28,8 @@ void setup()
 
 
   pinMode(16, OUTPUT);
-  pinMode(4, OUTPUT); //for light sensor...............................
-  pinMode(5, OUTPUT); //specifying D1 as an output pin
+  pinMode(4, OUTPUT); 
+  pinMode(5, OUTPUT); 
   Serial.begin(115200);
   delay(500);
   WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
@@ -83,7 +83,7 @@ void loop()
   if (n < 25)
   {
     digitalWrite(4, HIGH);
-  } else     digitalWrite(4, LOW);  //for light sensor...............................
+  } else     digitalWrite(4, LOW);  
 
 
   rgb = Firebase.getBool("RgbState/switch");
@@ -106,7 +106,7 @@ void loop()
   light = Firebase.getBool("LightState/switch");
   //Serial.println(light);
   if (light == true) {
-    digitalWrite(5, HIGH); //be careful the pin number on nodeMcu and arduino Ide are not the same eg. The used pin 5 in nodemcu is pin D1 not D5.
+    digitalWrite(5, HIGH); 
   }
   if (light == false) {
     digitalWrite(5, LOW);
